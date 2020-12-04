acc7c4647ff78a82714e4dc2f567eca287cb5cc1
Mario Leone <mleone10@gmail.com>
2020-12-04 01:23:50 -0500

    Day 4 - cleanup

--- cmd/day04/main.go
+++ cmd/day04/main.go
@@ -8,30 +8,63 @@ import (
 	"strings"
 )
 
-type passport map[string]string
+var hcls = map[string]bool{
+	"amb": true,
+	"blu": true,
+	"brn": true,
+	"gry": true,
+	"grn": true,
+	"hzl": true,
+	"oth": true,
+}
+
+type passport struct {
+	byr, iyr, eyr, hgt, hcl, ecl, pid, cid string
+	numFs                                  int
+}
 
 func main() {
 	ps := []passport{}
 	scanner := bufio.NewScanner(os.Stdin)
 
-	p := passport{}
+	acc := map[string]string{}
 	for scanner.Scan() {
 		fs := strings.Fields(scanner.Text())
 		if len(fs) == 0 {
-			ps = append(ps, p)
-			p = passport{}
+			// If no fields were extracted, this is an empty line
+			// Create a new passport with the accumulated fields, then reset the map
+			ps = append(ps, newPassport(acc))
+			acc = map[string]string{}
 			continue
 		}
 
 		for _, f := range fs {
-			p[strings.Split(f, ":")[0]] = strings.Split(f, ":")[1]
+			kv := strings.Split(f, ":")
+			acc[kv[0]] = kv[1]
 		}
 	}
+	// Last passport - we accumulated it, but never created it
+	ps = append(ps, newPassport(acc))
 
-	log.Printf("Valid passports: %d", countValidPassports(ps))
+	log.Printf("Valid passports by field count: %d", countValidPassports(ps, validateFields))
+	log.Printf("Valid passports by data: %d", countValidPassports(ps, validateData))
 }
 
-func countValidPassports(ps []passport) int {
+func newPassport(fs map[string]string) passport {
+	return passport{
+		byr:   fs["byr"],
+		iyr:   fs["iyr"],
+		eyr:   fs["eyr"],
+		hgt:   fs["hgt"],
+		hcl:   fs["hcl"],
+		ecl:   fs["ecl"],
+		pid:   fs["pid"],
+		cid:   fs["cid"],
+		numFs: len(fs),
+	}
+}
+
+func countValidPassports(ps []passport, isValid func(p passport) bool) int {
 	var sum int
 
 	for _, p := range ps {
@@ -44,59 +77,83 @@ func countValidPassports(ps []passport) int {
 	return sum
 }
 
-func isValid(p passport) bool {
-	ks := keys(p)
-	_, cidPresent := p["cid"]
-	return (len(ks) == 8 || (len(ks) == 7 && !cidPresent)) && allFieldsValid(p)
+func validateFields(p passport) bool {
+	return p.numFs == 8 || p.numFs == 7 && p.cid == ""
 }
 
-func allFieldsValid(p passport) bool {
-	if byr, _ := strconv.Atoi(p["byr"]); byr < 1920 || byr > 2002 {
+func validateData(p passport) bool {
+	return p.byrIsValid() &&
+		p.iyrIsValid() &&
+		p.eyrIsValid() &&
+		p.hgtIsValid() &&
+		p.hclIsValid() &&
+		p.eclIsValid() &&
+		p.pidIsValid()
+}
+
+func (p passport) byrIsValid() bool {
+	byr, _ := strconv.Atoi(p.byr)
+	return byr >= 1920 && byr <= 2002
+}
+
+func (p passport) iyrIsValid() bool {
+	iyr, _ := strconv.Atoi(p.iyr)
+	return iyr >= 2010 && iyr <= 2020
+}
+
+func (p passport) eyrIsValid() bool {
+	eyr, _ := strconv.Atoi(p.eyr)
+	return eyr >= 2020 && eyr <= 2030
+}
+
+func (p passport) hgtIsValid() bool {
+	if p.hgt == "" {
 		return false
 	}
-	if iyr, _ := strconv.Atoi(p["iyr"]); iyr < 2010 || iyr > 2020 {
-		return false
-	}
-	if eyr, _ := strconv.Atoi(p["eyr"]); eyr < 2020 || eyr > 2030 {
-		return false
-	}
-	if p["hgt"][len(p["hgt"])-2:] != "cm" && p["hgt"][len(p["hgt"])-2:] != "in" {
-		return false
-	}
-	if p["hgt"][len(p["hgt"])-2:] == "cm" {
-		if hgt, _ := strconv.Atoi(p["hgt"][:len(p["hgt"])-2]); hgt < 150 || hgt > 193 {
-			return false
+
+	lenHgt := len(p.hgt)
+	switch p.hgt[lenHgt-2:] {
+	case "cm":
+		if hgt, _ := strconv.Atoi(p.hgt[:lenHgt-2]); hgt >= 150 && hgt <= 193 {
+			return true
 		}
-	}
-	if p["hgt"][len(p["hgt"])-2:] == "in" {
-		if hgt, _ := strconv.Atoi(p["hgt"][:len(p["hgt"])-2]); hgt < 59 || hgt > 76 {
-			return false
+	case "in":
+		if hgt, _ := strconv.Atoi(p.hgt[:lenHgt-2]); hgt >= 59 && hgt <= 76 {
+			return true
 		}
-	}
-	if string(p["hcl"][0]) != "#" {
-		return false
-	} else {
-		_, err := strconv.ParseUint(p["hcl"][1:], 16, 64)
-		if err != nil {
-			return false
-		}
-	}
-	if p["ecl"] != "amb" && p["ecl"] != "blu" && p["ecl"] != "gry" && p["ecl"] != "grn" && p["ecl"] != "hzl" && p["ecl"] != "oth" && p["ecl"] != "brn" {
+	default:
 		return false
 	}
-	if len(p["pid"]) != 9 {
+	return false
+}
+
+func (p passport) hclIsValid() bool {
+	if p.hcl == "" {
 		return false
 	}
-	if _, err := strconv.Atoi(p["pid"]); err != nil {
+
+	if string(p.hcl[0]) != "#" {
 		return false
 	}
+	_, err := strconv.ParseUint(p.hcl[1:], 16, 64)
+	if err != nil {
+		return false
+	}
+
 	return true
 }
 
-func keys(p passport) []string {
-	var ks []string
-	for k := range p {
-		ks = append(ks, k)
+func (p passport) eclIsValid() bool {
+	return hcls[p.ecl]
+}
+
+func (p passport) pidIsValid() bool {
+	if len(p.pid) != 9 {
+		return false
 	}
-	return ks
+	if _, err := strconv.Atoi(p.pid); err != nil {
+		return false
+	}
+
+	return true
 }
