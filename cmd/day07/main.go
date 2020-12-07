f4b6180280095386eef8b3963822a823fd282dfd
Mario Leone <mleone10@gmail.com>
2020-12-07 13:54:18 -0500

    Day 7 - refactor

--- cmd/day07/main.go
+++ cmd/day07/main.go
@@ -9,49 +9,58 @@ import (
 	"strings"
 )
 
+const colorShinyGold = "shiny gold"
+
 var contentsMatch = regexp.MustCompile(`(?U:(\d+) (.+) bag[s]?)`)
 
-type rule struct {
-	color    string
-	contents []content
-}
+type color string
+type bags map[color]bag
+type contents map[color]int
 
-type content struct {
-	count int
-	color string
+type bag struct {
+	color    color
+	contents map[color]int
 }
 
 func main() {
-	rs := map[string]rule{}
+	bs := bags{}
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
-		ruleString := strings.Split(scanner.Text(), " bags contain ")
-		color := ruleString[0]
-		contentStrings := contentsMatch.FindAllStringSubmatch(ruleString[1], -1)
-		cs := []content{}
-		for _, c := range contentStrings {
+		b := bag{color(""), contents{}}
+		l := strings.Split(scanner.Text(), " bags contain ")
+		b.color = color(l[0])
+		for _, c := range contentsMatch.FindAllStringSubmatch(l[1], -1) {
 			count, _ := strconv.Atoi(c[1])
-			cs = append(cs, content{
-				count: count,
-				color: c[2],
-			})
+			b.contents[color(c[2])] = count
 		}
-		rs[color] = rule{
-			color:    color,
-			contents: cs,
-		}
+		bs[b.color] = b
 	}
 
-	log.Printf("Number of valid outer bags: %d", sumValidOuterBags(rs))
-	log.Printf("Number of bags inside shiny gold bag: %d", computeContentsOfOuterBagsP2(rs)["shiny gold"])
+	cs := computeContentsOfOuterBags(bs)
+	log.Printf("Number of valid outer bags: %d", sumValidOuterBags(colorShinyGold, cs))
+	log.Printf("Number of bags inside shiny gold bag: %d", getNumBagsInColor(colorShinyGold, cs))
 }
 
-func sumValidOuterBags(rs map[string]rule) int {
+func computeContentsOfOuterBags(bs bags) map[color]contents {
+	cs := map[color]contents{}
+
+	for _, b := range bs {
+		c := contents{}
+		for color, n := range traverseBag(b.color, bs) {
+			c[color] += n
+		}
+		cs[b.color] = c
+	}
+
+	return cs
+}
+
+func sumValidOuterBags(color color, cs map[color]contents) int {
 	var sum int
 
-	for _, contents := range computeContentsOfOuterBags(rs) {
-		if outerBagContainsShinyGold(contents) {
+	for _, cs := range cs {
+		if outerBagContainsColor(color, cs) {
 			sum++
 		}
 	}
@@ -59,55 +68,32 @@ func sumValidOuterBags(rs map[string]rule) int {
 	return sum
 }
 
-func computeContentsOfOuterBags(rs map[string]rule) map[string][]string {
-	bs := map[string][]string{}
-
-	for _, r := range rs {
-		bs[r.color] = traverseBag(r.color, rs)
-	}
-
-	return bs
-}
-
-func computeContentsOfOuterBagsP2(rs map[string]rule) map[string]int {
-	bs := map[string]int{}
-
-	for _, r := range rs {
-		for _, n := range traverseBagNum(r.color, rs) {
-			bs[r.color] += n
-		}
-	}
-
-	return bs
-}
-
-func outerBagContainsShinyGold(cs []string) bool {
-	for _, c := range cs {
-		if c == "shiny gold" {
+func outerBagContainsColor(color color, cs contents) bool {
+	for c := range cs {
+		if c == color {
 			return true
 		}
 	}
 	return false
 }
 
-func traverseBag(color string, rs map[string]rule) []string {
-	cs := []string{}
+func getNumBagsInColor(color color, contents map[color]contents) int {
+	var sum int
 
-	for _, c := range rs[color].contents {
-		cs = append(cs, c.color)
-		cs = append(cs, traverseBag(c.color, rs)...)
+	for _, num := range contents[color] {
+		sum += num
 	}
 
-	return cs
+	return sum
 }
 
-func traverseBagNum(color string, rs map[string]rule) map[string]int {
-	cs := map[string]int{}
+func traverseBag(color color, bs bags) contents {
+	cs := contents{}
 
-	for _, c := range rs[color].contents {
-		cs[c.color] += c.count
-		for color, num := range traverseBagNum(c.color, rs) {
-			cs[color] += num * c.count
+	for color, count := range bs[color].contents {
+		cs[color] += count
+		for color, num := range traverseBag(color, bs) {
+			cs[color] += num * count
 		}
 	}
 
