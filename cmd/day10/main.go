61833084aad58197dd8971ad10f9c515eaef5e15
Mario Leone <mleone10@gmail.com>
2020-12-10 19:58:54 -0500

    Day 10 - part 2

--- cmd/day10/main.go
+++ cmd/day10/main.go
@@ -22,7 +22,7 @@ func main() {
 	ints = append(ints, ints[len(ints)-1]+3)
 
 	log.Printf("Sum of 1-jolt diffs multiplied by sum of 3-jold diffs: %d", calcProductJoltDiffSums(ints))
-	log.Printf("Possible combinations of adapters: %d", computeCombinations(ints))
+	log.Printf("Possible combinations of adapters: %d", countRoutes(0, ints, map[int]int{}))
 }
 
 func calcProductJoltDiffSums(ints []int) int {
@@ -39,6 +39,28 @@ func calcProductJoltDiffSums(ints []int) int {
 	return ones * threes
 }
 
-func computeCombinations(ints []int) int {
-	return 0
+func countRoutes(index int, ints []int, visited map[int]int) int {
+	if index >= len(ints)-3 {
+		return 1
+	}
+
+	origin := ints[index]
+	if v, ok := visited[origin]; ok {
+		return v
+	}
+
+	var num int
+	for i := 1; i <= 3; i++ {
+		candidate := ints[index+i]
+		if match(origin, candidate) {
+			num += countRoutes(index+i, ints, visited)
+		}
+	}
+
+	visited[origin] = num
+	return num
+}
+
+func match(low, high int) bool {
+	return low+1 == high || low+2 == high || low+3 == high
 }
