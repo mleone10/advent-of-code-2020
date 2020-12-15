0f3a71d95c77d1a06015246eb78e62ca06cb7484
Mario Leone <mleone10@gmail.com>
2020-12-14 19:59:17 -0500

    Day 14 - part 2

--- cmd/day14/main.go
+++ cmd/day14/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"regexp"
 	"strconv"
@@ -12,66 +13,128 @@ import (
 
 var regexMem = regexp.MustCompile(`mem\[(\d*)\] = (\d*)`)
 
-type memory map[adr]val
 type adr int
 type val int
+type memory map[adr]val
 type mask string
-type masker struct {
-	mem memory
-	m   mask
+type valMasker struct {
+	memory
+	m mask
+}
+type adrMasker struct {
+	memory
+	m     mask
+	masks []mask
 }
 
 func main() {
-	var masker masker
+	var vm valMasker
+	var am adrMasker
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
 		switch l := scanner.Text(); strings.Contains(l, "mask") {
 		case true:
-			masker.setMask(mask(strings.Split(l, " = ")[1]))
+			m := strings.Split(l, " = ")[1]
+			vm.setMask(m)
+			am.setMask(m)
 		case false:
 			memLine := regexMem.FindAllStringSubmatch(l, -1)
 			a, _ := strconv.Atoi(memLine[0][1])
 			v, _ := strconv.Atoi(memLine[0][2])
-			masker.store(adr(a), val(v))
+			vm.store(adr(a), val(v))
+			am.store(adr(a), val(v))
 		}
 	}
 
-	log.Printf("Sum of memory after all operations: %d", masker.sum())
+	log.Printf("Sum of memory after value masking: %d", vm.sum())
+	log.Printf("Sum of memory after address masking: %d", am.sum())
 }
 
-func (masker *masker) setMask(m mask) {
-	masker.m = m
-}
-
-func (masker *masker) store(a adr, v val) {
-	if masker.mem == nil {
-		masker.mem = make(memory)
+func (m *valMasker) store(a adr, v val) {
+	if m.memory == nil {
+		m.memory = make(memory)
 	}
 
-	masker.mem[a] = masker.mask(v)
+	m.memory.store(a, m.maskVal(v))
 }
 
-func (masker masker) mask(v val) val {
-	b := fmt.Sprintf("%036b", int(v))
+func (m *valMasker) setMask(s string) {
+	m.m = mask(s)
+}
+
+func (m valMasker) maskVal(v val) val {
+	bVal := fmt.Sprintf("%036b", int(v))
 	var newB string
-	for i, m := range masker.m {
-		switch m {
+	for i, b := range m.m {
+		switch b {
 		case '0':
 			newB += "0"
 		case '1':
 			newB += "1"
 		case 'X':
-			newB += string(b[i])
+			newB += string(bVal[i])
 		}
 	}
 	newV, _ := strconv.ParseInt(newB, 2, 64)
 	return val(newV)
 }
 
-func (masker masker) sum() int {
+func (m *adrMasker) store(a adr, v val) {
+	if m.memory == nil {
+		m.memory = make(memory)
+	}
+
+	for _, ma := range m.maskAdr(a) {
+		m.memory.store(ma, v)
+	}
+}
+
+func (m *adrMasker) maskAdr(a adr) []adr {
+	return m.genAdrPermutations(m.applyMask(a))
+}
+
+func (m *adrMasker) applyMask(a adr) mask {
+	bAdr := fmt.Sprintf("%036b", int(a))
+	var nbAdr string
+	for i, b := range m.m {
+		switch b {
+		case '0':
+			nbAdr += string(bAdr[i])
+		default:
+			nbAdr += string(b)
+		}
+	}
+	return mask(nbAdr)
+}
+
+func (m *adrMasker) genAdrPermutations(a mask) []adr {
+	as := []adr{}
+	xs := strings.Count(string(a), "X")
+	mx := int(math.Pow(2.0, float64(xs)))
+	for i := 0; i < mx; i++ {
+		bX := fmt.Sprintf("%0*b", len(fmt.Sprintf("%b", mx-1)), i)
+		sAdr := string(a)
+		for _, b := range bX {
+			sAdr = strings.Replace(sAdr, "X", string(b), 1)
+		}
+		dAdr, _ := strconv.ParseInt(sAdr, 2, 64)
+		as = append(as, adr(dAdr))
+	}
+	return as
+}
+
+func (m *adrMasker) setMask(s string) {
+	m.m = mask(s)
+}
+
+func (m memory) store(a adr, v val) {
+	m[a] = v
+}
+
+func (m memory) sum() int {
 	var sum int
-	for _, v := range masker.mem {
+	for _, v := range m {
 		sum += int(v)
 	}
 	return sum
